e07d04cb7a2bfb87d5aa042d87bb82bede50bf5f
Prateek <sharma.prateek95@gmail.com>
2017-06-14 22:54:57 +0530

    initial

---
+++ src/main/java/com/example/UserController.java
@@ -0,0 +1,50 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private UserRepository repository;
+
+    @Autowired
+    public UserController(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserData> get(@PathVariable("id") Long id) {
+        UserData user = repository.findOne(id);
+        if (null == user) {
+            return new ResponseEntity<UserData>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<UserData>(user, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/addnew", method = RequestMethod.POST)
+    public ResponseEntity<UserData> update(@RequestBody UserData user) {
+        repository.save(user);
+        return get(user.getId());
+    }
+
+    @RequestMapping(value = "/result", method = RequestMethod.GET)
+    public List<UserData> all() {
+        return repository.findAll();
+    }
+
+    @RequestMapping("/datacheck")
+    @ResponseBody
+    String data() {
+        return "check data";
+    }
+}
---
+++ src/main/java/com/example/UserData.java
@@ -0,0 +1,69 @@
+package com.example;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "userdata")
+public class UserData {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @NotEmpty
+    @Column(name = "name")
+    private String name;
+
+    @NotEmpty
+    @Column(name = "email")
+    private String email;
+
+    @NotEmpty
+    @Column(name = "password")
+    private String password;
+
+    public UserData() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
