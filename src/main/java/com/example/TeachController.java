9127c8200bf50f4b7094f9fd00a1b73e2103cbf8
Prateek <sharma.prateek95@gmail.com>
2017-06-15 22:24:30 +0530

    initial

---
+++ src/main/java/com/example/TeachController.java
@@ -0,0 +1,52 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * Created by Prateek on 6/15/2017.
+ */
+
+public class TeachController {
+
+    private TeachRepository repository;
+
+    @Autowired
+    public TeachController(TeachRepository repository) {
+        this.repository = repository;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<TeacherData> get(@PathVariable("id") Long id) {
+        TeacherData user = repository.findOne(id);
+        if (null == user) {
+            return new ResponseEntity<TeacherData>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<TeacherData>(user, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/addnew2", method = RequestMethod.POST)
+    public ResponseEntity<TeacherData> update(@RequestBody TeacherData user) {
+        repository.save(user);
+        return get(user.getId());
+    }
+
+    @RequestMapping(value = "/result2", method = RequestMethod.GET)
+    public List<TeacherData> all() {
+        return repository.findAll();
+    }
+
+    @RequestMapping("/datacheckteach")
+    @ResponseBody
+    String data() {
+        return "In teacher Data";
+    }
+}
